d1899764e8354452bacc9e63438b66083a4918eb
Angel <angeldrc1612@gmail.com>
2025-04-04 11:26:50 -0500

    ciclos for y while

---
+++ ciclo_for.py
@@ -0,0 +1,21 @@
+#Ciclo for 
+#especial para recorrer 
+#Estrucutras de datos 
+#Todo lo que esta entre 
+#[] Se denomia una lista 
+
+
+# Funcion range (python)
+#Crea una lista de numeros en el rango definido 
+# por el usuario
+
+
+numero = int(input("Ingrese un número: "))
+
+for i in range(10, 0,-1): 
+    resultado = numero * i
+    print(numero, "X", i, "=", resultado)
+
+    
+    
+    
---
+++ ciclo_while.py
@@ -0,0 +1,26 @@
+#EJERCICO 1 
+#imprimir la tabla de multiplicar del numero 
+# que un usuario ingrese por teclado 
+# utilizando un ciclo 
+# while
+
+numero  = int(input("Ingrese un numero: "))
+i=1
+while 1<=10:
+    resultado = numero*1
+    print(numero,"x", i,"=", numero)
+    i = i + 1
+
+
+
+#variable contadora
+
+i=5
+
+while i<=25: 
+    print(i)
+
+ ##Instruccion de incremento 
+    i = i + 5
+    
+    
